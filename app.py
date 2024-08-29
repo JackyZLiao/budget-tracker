dbe79fe24496089a3a485f2a63e36b49d45a6ff4
Jacky <z5209155@ad.unsw.edu.au>
2024-08-29 13:11:58 +1000

    pie-chart implemented

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 from local_components import card_container
 import up_api_helpers as up
 import helpers as h
+import plotly.express as px
 
 # **************************************** SQL DB Setup **************************************** #
 
@@ -170,132 +171,156 @@ df['week'] = df['date_time'].dt.strftime('%Y-W%W')
 df['month'] = df['date_time'].dt.strftime('%B %Y')
 df = df[df['week'] == period] if view == 'Weekly' else df[df['month'] == period]
 df.drop(columns=['date_time', 'week', 'month'], inplace=True)
-
+print(df)
 with card_container():
     ui.table(data=df)
 
 # **************************************** Budget **************************************** #
 
-budget_spent = df.groupby('category')['amount'].sum()
-print(budget_spent)
-budget_multiplier = 1 if view == 'Weekly' else 4
+categories = df.groupby('category')['amount'].sum()
+categories = categories.reset_index()
+categories.columns = ['category', 'amount'];
 
-r_t_target = budget_set[budget_set['name'] == 'Restaurants & Takeaway']['budget'].iloc[0] * budget_multiplier
-c_a_target = budget_set[budget_set['name'] == 'Clothing & Accessories']['budget'].iloc[0] * budget_multiplier
-h_b_target = budget_set[budget_set['name'] == 'Health & Beauty']['budget'].iloc[0] * budget_multiplier
-s_m_target = budget_set[budget_set['name'] == 'Subscriptions & Memberships']['budget'].iloc[0] * budget_multiplier
-e_r_target = budget_set[budget_set['name'] == 'Entertainment & Recreation']['budget'].iloc[0] * budget_multiplier
-la_target = budget_set[budget_set['name'] == 'Life Admin']['budget'].iloc[0] * budget_multiplier
-g_target = budget_set[budget_set['name'] == 'Groceries']['budget'].iloc[0] * budget_multiplier
-t_target = budget_set[budget_set['name'] == 'Transportation']['budget'].iloc[0] * budget_multiplier
-s_target = budget_set[budget_set['name'] == 'Shopping']['budget'].iloc[0] * budget_multiplier
-o_target = budget_set[budget_set['name'] == 'Other']['budget'].iloc[0] * budget_multiplier
+st.write(f'## Spending by Category')
 
-r_t_spent = round(budget_spent['Restaurants & Takeaway'], 2) if 'Restaurants & Takeaway' in budget_spent.index else 0
-c_a_spent = round(budget_spent['Clothing & Accessories'], 2) if 'Clothing & Accessories' in budget_spent.index else 0
-h_b_spent = round(budget_spent['Health & Beauty'], 2) if 'Health & Beauty' in budget_spent.index else 0
-s_m_spent = round(budget_spent['Subscriptions & Memberships'], 2) if 'Subscriptions & Memberships' in budget_spent.index else 0
-e_r_spent = round(budget_spent['Entertainment & Recreation'], 2) if 'Entertainment & Recreation' in budget_spent.index else 0
-la_spent = round(budget_spent['Life Admin'], 2) if 'Life Admin' in budget_spent.index else 0
-g_spent = round(budget_spent['Groceries'], 2) if 'Groceries' in budget_spent.index else 0
-t_spent = round(budget_spent['Transportation'], 2) if 'Transportation' in budget_spent.index else 0
-s_spent = round(budget_spent['Shopping'], 2) if 'Shopping' in budget_spent.index else 0
-o_spent = round(budget_spent['Other'], 2) if 'Other' in budget_spent.index else 0
-
-colour = 'green'
 with card_container():
-    cols = st.columns(2)
-    with cols[0]:
-        colour = 'red' if r_t_spent > r_t_target else 'green'
-        st.write(f'##### Restaurants and Takeaway: :{colour}[\${r_t_spent}] out of ${r_t_target}')
-        st.progress(r_t_spent / r_t_target if r_t_spent < r_t_target else 0 if r_t_target == 0 else 1.0)
+    fig = px.pie(categories, values='amount', names='category')
 
-        st.write('')
-        colour = 'red' if c_a_spent > c_a_target else 'green'
-        st.write(f'##### Clothing & Accessories: :{colour}[\${c_a_spent}] out of ${c_a_target}')
-        st.progress(c_a_spent / c_a_target if c_a_spent < c_a_target else 0 if c_a_target == 0 else 1.0)
+    fig.update_traces(
+        hole=0.5,
+        domain=dict(x=[0, 0.5], y=[0, 1])
+    )
+    fig.update_layout(
+        legend_font_size=20,  # Legend font size
+        font=dict(size=18),  # General font size (for labels)
+        legend=dict(
+            orientation="v",  # Horizontal legend
+            yanchor="middle",  # Anchor the legend at the bottom
+            y=0.5,  # Push the legend below the chart
+            xanchor="left",  # Center horizontally
+            x=0.6  # Center the legend
+        ),
+    )
+    st.plotly_chart(fig, use_container_width=True)
 
-        st.write('')
-        colour = 'red' if h_b_spent > h_b_target else 'green'
-        st.write(f'##### Health & Beauty: :{colour}[\${h_b_spent}] out of ${h_b_target}')
-        st.progress(h_b_spent / h_b_target if h_b_spent < h_b_target else 0 if h_b_target == 0 else 1.0)
 
-        st.write('')
-        colour = 'red' if s_m_spent > s_m_target else 'green'
-        st.write(f'##### Subscriptions & Memberships: :{colour}[\${s_m_spent}] out of ${s_m_target}')
-        st.progress(s_m_spent / s_m_target if s_m_spent < s_m_target else 0 if s_m_target == 0 else 1.0)
+#budget_multiplier = 1 if view == 'Weekly' else 4
+# r_t_target = budget_set[budget_set['name'] == 'Restaurants & Takeaway']['budget'].iloc[0] * budget_multiplier
+# c_a_target = budget_set[budget_set['name'] == 'Clothing & Accessories']['budget'].iloc[0] * budget_multiplier
+# h_b_target = budget_set[budget_set['name'] == 'Health & Beauty']['budget'].iloc[0] * budget_multiplier
+# s_m_target = budget_set[budget_set['name'] == 'Subscriptions & Memberships']['budget'].iloc[0] * budget_multiplier
+# e_r_target = budget_set[budget_set['name'] == 'Entertainment & Recreation']['budget'].iloc[0] * budget_multiplier
+# la_target = budget_set[budget_set['name'] == 'Life Admin']['budget'].iloc[0] * budget_multiplier
+# g_target = budget_set[budget_set['name'] == 'Groceries']['budget'].iloc[0] * budget_multiplier
+# t_target = budget_set[budget_set['name'] == 'Transportation']['budget'].iloc[0] * budget_multiplier
+# s_target = budget_set[budget_set['name'] == 'Shopping']['budget'].iloc[0] * budget_multiplier
+# o_target = budget_set[budget_set['name'] == 'Other']['budget'].iloc[0] * budget_multiplier
 
-        st.write('')
-        colour = 'red' if g_spent > g_target else 'green'
-        st.write(f'##### Groceries: :{colour}[\${g_spent}] out of ${g_target}')
-        st.progress(g_spent / g_target if g_spent < g_target else 0 if g_spent == 0 else 1.0)
-    with cols[1]:
-        colour = 'red' if t_spent > t_target else 'green'
-        st.write(f'##### Transportation: :{colour}[\${t_spent}] out of ${t_target}')
-        st.progress(t_spent / t_target if t_spent < t_target and t_spent > 0 else 0 if t_spent == 0 else 1.0)
+# r_t_spent = round(budget_spent['Restaurants & Takeaway'], 2) if 'Restaurants & Takeaway' in budget_spent.index else 0
+# c_a_spent = round(budget_spent['Clothing & Accessories'], 2) if 'Clothing & Accessories' in budget_spent.index else 0
+# h_b_spent = round(budget_spent['Health & Beauty'], 2) if 'Health & Beauty' in budget_spent.index else 0
+# s_m_spent = round(budget_spent['Subscriptions & Memberships'], 2) if 'Subscriptions & Memberships' in budget_spent.index else 0
+# e_r_spent = round(budget_spent['Entertainment & Recreation'], 2) if 'Entertainment & Recreation' in budget_spent.index else 0
+# la_spent = round(budget_spent['Life Admin'], 2) if 'Life Admin' in budget_spent.index else 0
+# g_spent = round(budget_spent['Groceries'], 2) if 'Groceries' in budget_spent.index else 0
+# t_spent = round(budget_spent['Transportation'], 2) if 'Transportation' in budget_spent.index else 0
+# s_spent = round(budget_spent['Shopping'], 2) if 'Shopping' in budget_spent.index else 0
+# o_spent = round(budget_spent['Other'], 2) if 'Other' in budget_spent.index else 0
 
-        st.write('')
-        colour = 'red' if s_spent > s_target else 'green'
-        st.write(f'##### Shopping: :{colour}[\${s_spent}] out of ${s_target}')
-        st.progress(s_spent / s_target if s_spent < s_target else 0 if s_spent == 0 else 1.0)
+# colour = 'green'
+# with card_container():
+#     cols = st.columns(2)
+#     with cols[0]:
+#         colour = 'red' if r_t_spent > r_t_target else 'green'
+#         st.write(f'##### Restaurants and Takeaway: :{colour}[\${r_t_spent}] out of ${r_t_target}')
+#         st.progress(r_t_spent / r_t_target if r_t_spent < r_t_target else 0 if r_t_target == 0 else 1.0)
 
-        st.write('')
-        colour = 'red' if la_spent > la_target else 'green'
-        st.write(f'##### Life Admin: :{colour}[\${la_spent}] out of ${la_target}')
-        st.progress(la_spent / la_target if la_spent < la_target else 0 if la_spent == 0 else 1.0)
+#         st.write('')
+#         colour = 'red' if c_a_spent > c_a_target else 'green'
+#         st.write(f'##### Clothing & Accessories: :{colour}[\${c_a_spent}] out of ${c_a_target}')
+#         st.progress(c_a_spent / c_a_target if c_a_spent < c_a_target else 0 if c_a_target == 0 else 1.0)
 
-        st.write('')
-        colour = 'red' if e_r_spent > e_r_target else 'green'
-        st.write(f'##### Entertain & Recreation: :{colour}[\${e_r_spent}] out of ${e_r_target}')
-        st.progress(e_r_spent / e_r_target if e_r_spent < e_r_target else 0 if e_r_spent == 0 else 1.0)
+#         st.write('')
+#         colour = 'red' if h_b_spent > h_b_target else 'green'
+#         st.write(f'##### Health & Beauty: :{colour}[\${h_b_spent}] out of ${h_b_target}')
+#         st.progress(h_b_spent / h_b_target if h_b_spent < h_b_target else 0 if h_b_target == 0 else 1.0)
 
-        st.write('')
-        colour = 'red' if o_spent > o_target else 'green'
-        st.write(f'##### Other: :{colour}[\${o_spent}] out of ${o_target}')
-        st.progress(o_spent / o_target if o_spent < o_target else 0 if o_target == 0 else 1.0)
+#         st.write('')
+#         colour = 'red' if s_m_spent > s_m_target else 'green'
+#         st.write(f'##### Subscriptions & Memberships: :{colour}[\${s_m_spent}] out of ${s_m_target}')
+#         st.progress(s_m_spent / s_m_target if s_m_spent < s_m_target else 0 if s_m_target == 0 else 1.0)
+
+#         st.write('')
+#         colour = 'red' if g_spent > g_target else 'green'
+#         st.write(f'##### Groceries: :{colour}[\${g_spent}] out of ${g_target}')
+#         st.progress(g_spent / g_target if g_spent < g_target else 0 if g_spent == 0 else 1.0)
+#     with cols[1]:
+#         colour = 'red' if t_spent > t_target else 'green'
+#         st.write(f'##### Transportation: :{colour}[\${t_spent}] out of ${t_target}')
+#         st.progress(t_spent / t_target if t_spent < t_target and t_spent > 0 else 0 if t_spent == 0 else 1.0)
+
+#         st.write('')
+#         colour = 'red' if s_spent > s_target else 'green'
+#         st.write(f'##### Shopping: :{colour}[\${s_spent}] out of ${s_target}')
+#         st.progress(s_spent / s_target if s_spent < s_target else 0 if s_spent == 0 else 1.0)
+
+#         st.write('')
+#         colour = 'red' if la_spent > la_target else 'green'
+#         st.write(f'##### Life Admin: :{colour}[\${la_spent}] out of ${la_target}')
+#         st.progress(la_spent / la_target if la_spent < la_target else 0 if la_spent == 0 else 1.0)
+
+#         st.write('')
+#         colour = 'red' if e_r_spent > e_r_target else 'green'
+#         st.write(f'##### Entertain & Recreation: :{colour}[\${e_r_spent}] out of ${e_r_target}')
+#         st.progress(e_r_spent / e_r_target if e_r_spent < e_r_target else 0 if e_r_spent == 0 else 1.0)
+
+#         st.write('')
+#         colour = 'red' if o_spent > o_target else 'green'
+#         st.write(f'##### Other: :{colour}[\${o_spent}] out of ${o_target}')
+#         st.progress(o_spent / o_target if o_spent < o_target else 0 if o_target == 0 else 1.0)
     
-    colour = 'red' if round(budget_spent.sum(), 2) > budget else 'green'
-    st.write('')
-    st.write(f'## Total Spending: :{colour}[\${round(budget_spent.sum(), 2)}] out of \${budget}')
+#     colour = 'red' if round(budget_spent.sum(), 2) > budget else 'green'
+#     st.write('')
+#     st.write(f'## Total Spending: :{colour}[\${round(budget_spent.sum(), 2)}] out of \${budget}')
 
-budget_targets = {}
-with st.expander("**Edit Weekly Budget Targets**"):
-    cols = st.columns(5)
-    with cols[0]:
-        budget_targets['Restaurants & Takeaway'] = st.number_input('**Restaurants & Takeaway**', value=r_t_target)
-        budget_targets['Transportation'] = st.number_input('**Transportation**', value=t_target)
-    with cols[1]:
-        budget_targets['Life Admin'] = st.number_input('**Life Admin**', value=la_target)
-        budget_targets['Subscriptions & Memberships'] = st.number_input('**Subscriptions & Memberships**', value=s_m_target)
-    with cols[2]:
-        budget_targets['Clothing & Accessories'] = st.number_input('**Clothing & Accessories**', value=c_a_target)
-        budget_targets['Shopping'] = st.number_input('**Shopping**', value=s_target)
-    with cols[3]:
-        budget_targets['Health & Beauty'] = st.number_input('**Health & Beauty**', value=h_b_target)
-        budget_targets['Entertainment & Recreation'] = st.number_input('**Entertainment & Recreation**', value=e_r_target)
-    with cols[4]:
-        budget_targets['Groceries'] = st.number_input('**Groceries**', value=g_target)
-        budget_targets['Other'] = st.number_input('**Other**', value=o_target)
-    st.write('')
+# budget_targets = {}
+# with st.expander("**Edit Weekly Budget Targets**"):
+#     cols = st.columns(5)
+#     with cols[0]:
+#         budget_targets['Restaurants & Takeaway'] = st.number_input('**Restaurants & Takeaway**', value=r_t_target)
+#         budget_targets['Transportation'] = st.number_input('**Transportation**', value=t_target)
+#     with cols[1]:
+#         budget_targets['Life Admin'] = st.number_input('**Life Admin**', value=la_target)
+#         budget_targets['Subscriptions & Memberships'] = st.number_input('**Subscriptions & Memberships**', value=s_m_target)
+#     with cols[2]:
+#         budget_targets['Clothing & Accessories'] = st.number_input('**Clothing & Accessories**', value=c_a_target)
+#         budget_targets['Shopping'] = st.number_input('**Shopping**', value=s_target)
+#     with cols[3]:
+#         budget_targets['Health & Beauty'] = st.number_input('**Health & Beauty**', value=h_b_target)
+#         budget_targets['Entertainment & Recreation'] = st.number_input('**Entertainment & Recreation**', value=e_r_target)
+#     with cols[4]:
+#         budget_targets['Groceries'] = st.number_input('**Groceries**', value=g_target)
+#         budget_targets['Other'] = st.number_input('**Other**', value=o_target)
+#     st.write('')
 
-    if ui.button('Set Budget', variant='outline', key='outline'):
-        for key, value in budget_targets.items():
-            update_statement = '''
-                UPDATE budget
-                SET budget = ?
-                WHERE name = ?
-            '''
-            cursor.execute(update_statement, (value, key))
-        conn.commit()
-st.markdown(
-    """
-    <style>
-        .stProgress > div > div > div > div {
-            background-image: linear-gradient(to right, black , black);
-        }
-    </style>""",
-    unsafe_allow_html=True,
-)
+#     if ui.button('Set Budget', variant='outline', key='outline'):
+#         for key, value in budget_targets.items():
+#             update_statement = '''
+#                 UPDATE budget
+#                 SET budget = ?
+#                 WHERE name = ?
+#             '''
+#             cursor.execute(update_statement, (value, key))
+#         conn.commit()
+# st.markdown(
+#     """
+#     <style>
+#         .stProgress > div > div > div > div {
+#             background-image: linear-gradient(to right, black , black);
+#         }
+#     </style>""",
+#     unsafe_allow_html=True,
+# )
 
 # **************************************** Add Expenses **************************************** #
 outer_cols = st.columns(2)
